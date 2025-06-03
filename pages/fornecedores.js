7ee8844a34e98c24b5fd475fbbfa418830464b8e
TomRodrigues44 <tom.santanna@gmail.com>
2025-06-03 18:49:34 -0400

    Create fornecedores.js

---
+++ pages/fornecedores.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+
+export default function Fornecedores() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ padding: 20 }}>
+        <h2>Fornecedores</h2>
+        <p>Em breve: cadastro e lista de fornecedores!</p>
+      </div>
+    </div>
+  );
+}
