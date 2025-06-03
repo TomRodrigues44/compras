638c6476f0c1e79259d0c184b2bb2720afcea77d
TomRodrigues44 <tom.santanna@gmail.com>
2025-06-03 18:49:17 -0400

    Create produtos.js

---
+++ pages/produtos.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+
+export default function Produtos() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ padding: 20 }}>
+        <h2>Produtos</h2>
+        <p>Em breve: cadastro e lista de produtos!</p>
+      </div>
+    </div>
+  );
+}
