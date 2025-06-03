b299cb5337cfe3c15647eecdaf9276da253f3b1b
TomRodrigues44 <tom.santanna@gmail.com>
2025-06-03 18:49:51 -0400

    Create lista.js

---
+++ pages/lista.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+
+export default function Lista() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ padding: 20 }}>
+        <h2>Lista de Compras</h2>
+        <p>Em breve: selecione produtos para gerar sua lista diária!</p>
+      </div>
+    </div>
+  );
+}
